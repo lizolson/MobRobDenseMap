f422e2900f89e11456b3b4eefd8a54f21b7eec61
lizolson1 <lizolson@umich.edu>
2019-04-13 04:47:27 +0000

    commented

--- sgm.py
+++ sgm.py
@@ -10,6 +10,8 @@ import csv
 def main():
   leftdir = sys.argv[1]
   rightdir = sys.argv[2]
+  #Reading in Sprites points in the global frame. Creating an array data, where each row is for a timestep, and contains a 2d array of the
+  #data for that time step. 
   with open('globpts.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     next(csv_reader)
@@ -17,23 +19,25 @@ def main():
     for row in csv_reader:
         step = int(row[0])
         data[step].append(row[4:])
-  cloudfull = np.array([])
   for file in os.listdir(leftdir):
-    print(file)
     if file[-3:] != 'png':
       continue
     time = int(file[-8:-4])
-    if time > 60:
+    #Temporary time sequences to process
+    if time > 120:
         continue
     if time <11:
         continue
-    #print(data[time])
     ulist = [int(float(x[0])) for x in data[time]]
     xyz = np.zeros((100, 3))
     xyzp = np.zeros((100, 3))
-    print(len(ulist))
-    if len(ulist) <= 7:
+    # We need there to be at least 8 points, chose to have more, as it better removes outliers
+    if len(ulist) <= 9:
+    #if len(ulist) <= 7:
         continue
+    print(file)
+    print(len(ulist))
+    #Computing the disparity map with SGM
     leftimg = cv2.imread(leftdir + file)
     rightimg = cv2.imread(rightdir + file)
     bl = 0.537
@@ -55,12 +59,12 @@ def main():
             P2 = 32*3*window_size**2,
             fullDP = 1)#False)
     disp = stereo.compute(gray0, gray1).astype(np.float32) / 16.0
-    #cv2.imwrite('temp.png', disp)
-    #print(disp.shape)
-    #print(width, height)
+    #Cloud: will contain the x, y, z points
+    #colors: the color of the points
     cloud = np.zeros((width * height, 3))
-    #cloudperf = np.zeros((width * height, 3))
     colors = np.zeros((width * height, 3))
+    #U and V are the coordinates of the feature. I convert them to the x' y' z' in my frame, store them in xyzp. 
+    #Sprite's points in the global frame are in xyz
     for i in range(len(ulist)):
         u = int(float(data[time][i][0]))
         v = int(float(data[time][i][1]))
@@ -76,54 +80,36 @@ def main():
         y = float(data[time][i][3])
         z = float(data[time][i][4])
         xyz[i] = [x, y, z]
-    #print('xyz', xyz)
-    #print('xyzp', xyzp)
-    print('val check')
+    #print('val check')
     #print(np.linalg.norm(np.array((xyz[0, 0], xyz[0, 1], xyz[0, 2]))-np.array((xyz[1, 0], xyz[1, 1], xyz[1,2]))))
     #print(np.linalg.norm(np.array((xyzp[0, 0], xyzp[0, 1], xyzp[0, 2]))-np.array((xyzp[1, 0], xyzp[1, 1], xyzp[1,2]))))
+    #Removing the empy rows, that didn't have any points
     xyz = xyz[~np.all(xyz==0, axis = 1)]
     xyz = xyz.T
-    #print('xyz', xyz)
     xyzp = xyzp[~np.all(xyzp==0, axis = 1)]
     xyzp = xyzp.T
-    #print('xyz', xyz)
-    #print('xyzp', xyzp)
     n = len(xyz[0])
-    #print(n)
     
-    #myH = bestHomogeneousTransformation(xyz, xyz, len(xyz[0]))
     myH = bestHomogeneousTransformation(xyzp, xyz, len(xyz[0]))
     print(np.linalg.det(myH))
-    #print(myH) 
+    #Threshold of which points to add to final cloud. higher d threshold, closer up points have to be for consideration
     for c in range(width):
         for r in range(height):
             u = c - (width/2)
             v = r - (height/2)
-            #print(r)
             d = disp[r,c]
-            #print(d)
-            if d > 50:
+            if d > 20:
                 z = focal * bl /d
                 #z = 1
                 x = u * z/ focal
                 y = v *z / focal
                 homg = np.array([[x],[y],[z],[1.0]])
-                #out = np.array(myH) @ homg
                 out = myH.dot(homg)
-                #print('test')
-                #print(homg)
-                #print(out)
-                #print([out[0,0], out[1,0], out[2,0]])
-                #print(out)
-                #x = u * z / focal
-                #y = v * z / focal
                 cloud[c * height + r] = [out[0,0], out[1,0], out[2,0]]
-                #cloudperf[c * height + r] = [x, y, z]
-                #print(out[0])
                 color = leftimg[r, c] / 255.0
                 colors[c * height + r] = [color[2],color[1] ,color[0]]
+    #Clear out array rows that are empty
     cloud = cloud[~np.all(cloud==0, axis = 1)]
-    #cloudperf = cloudperf[~np.all(cloud==0, axis = 1)]
     colors = colors[~np.all(colors==0, axis = 1)]
     pcd = PointCloud()
     pcd.points = Vector3dVector(cloud)
