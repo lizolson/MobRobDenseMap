db0289d0698d45724feffb3a2293f815deebc66e
lizolson1 <lizolson@umich.edu>
2019-04-13 03:53:42 +0000

    sgm changes

--- sgm.py
+++ sgm.py
@@ -19,14 +19,20 @@ def main():
         data[step].append(row[4:])
   cloudfull = np.array([])
   for file in os.listdir(leftdir):
+    print(file)
     if file[-3:] != 'png':
       continue
     time = int(file[-8:-4])
+    if time > 60:
+        continue
+    if time <11:
+        continue
     #print(data[time])
     ulist = [int(float(x[0])) for x in data[time]]
-    xyz = np.zeros((30, 3))
-    xyzp = np.zeros((30, 3))
-    if len(ulist) < 5:
+    xyz = np.zeros((100, 3))
+    xyzp = np.zeros((100, 3))
+    print(len(ulist))
+    if len(ulist) <= 7:
         continue
     leftimg = cv2.imread(leftdir + file)
     rightimg = cv2.imread(rightdir + file)
@@ -53,14 +59,18 @@ def main():
     #print(disp.shape)
     #print(width, height)
     cloud = np.zeros((width * height, 3))
+    #cloudperf = np.zeros((width * height, 3))
     colors = np.zeros((width * height, 3))
     for i in range(len(ulist)):
         u = int(float(data[time][i][0]))
         v = int(float(data[time][i][1]))
         d = disp[v, u]
+        if d < 10:
+            continue
         zp = focal * bl /d
         xp = u * zp/ focal
         yp = v * zp / focal
+        
         xyzp[i] = [xp, yp, zp]
         x = float(data[time][i][2])
         y = float(data[time][i][3])
@@ -69,8 +79,8 @@ def main():
     #print('xyz', xyz)
     #print('xyzp', xyzp)
     print('val check')
-    print(np.linalg.norm(np.array((xyz[0, 0], xyz[0, 1], xyz[0, 2]))-np.array((xyz[1, 0], xyz[1, 1], xyz[1,2]))))
-    print(np.linalg.norm(np.array((xyzp[0, 0], xyzp[0, 1], xyzp[0, 2]))-np.array((xyzp[1, 0], xyzp[1, 1], xyzp[1,2]))))
+    #print(np.linalg.norm(np.array((xyz[0, 0], xyz[0, 1], xyz[0, 2]))-np.array((xyz[1, 0], xyz[1, 1], xyz[1,2]))))
+    #print(np.linalg.norm(np.array((xyzp[0, 0], xyzp[0, 1], xyzp[0, 2]))-np.array((xyzp[1, 0], xyzp[1, 1], xyzp[1,2]))))
     xyz = xyz[~np.all(xyz==0, axis = 1)]
     xyz = xyz.T
     #print('xyz', xyz)
@@ -81,8 +91,9 @@ def main():
     n = len(xyz[0])
     #print(n)
     
-    myH = bestHomogeneousTransformation(xyz, xyz, len(xyz[0]))
-    #myH = bestHomogeneousTransformation(xyzp, xyz, len(xyz[0]))
+    #myH = bestHomogeneousTransformation(xyz, xyz, len(xyz[0]))
+    myH = bestHomogeneousTransformation(xyzp, xyz, len(xyz[0]))
+    print(np.linalg.det(myH))
     #print(myH) 
     for c in range(width):
         for r in range(height):
@@ -91,7 +102,7 @@ def main():
             #print(r)
             d = disp[r,c]
             #print(d)
-            if d > 10:
+            if d > 50:
                 z = focal * bl /d
                 #z = 1
                 x = u * z/ focal
@@ -107,10 +118,12 @@ def main():
                 #x = u * z / focal
                 #y = v * z / focal
                 cloud[c * height + r] = [out[0,0], out[1,0], out[2,0]]
+                #cloudperf[c * height + r] = [x, y, z]
                 #print(out[0])
                 color = leftimg[r, c] / 255.0
                 colors[c * height + r] = [color[2],color[1] ,color[0]]
     cloud = cloud[~np.all(cloud==0, axis = 1)]
+    #cloudperf = cloudperf[~np.all(cloud==0, axis = 1)]
     colors = colors[~np.all(colors==0, axis = 1)]
     pcd = PointCloud()
     pcd.points = Vector3dVector(cloud)
